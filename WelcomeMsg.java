9408ff5a6931f5b39719b22d7765d695f71a60c9
SateeshB1997 <bapanapallisateesh1997@gmail.com>
2025-11-13 11:50:51 +0530

    code

---
+++ WelcomeMsg.java
@@ -0,0 +1,12 @@
+public class WelcomeMsg {
+    public static void main(String[] args) {
+        // System.out.print("Hello, World.\nHello, Java.");
+       
+    /*    
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    */
+
+        System.out.println("Hello, World.\nHello, Java.");
+    }
+}
